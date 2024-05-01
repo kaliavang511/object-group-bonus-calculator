66ca7084e1eaf8bd1a31e1a84885640c080e1aac
Kalia Vang <kaliavang511@gmail.com>
2024-04-30 22:14:10 -0500

    went over eveyrthing

--- client.js
+++ client.js
@@ -32,8 +32,6 @@ const employees = [
   }
 ];
 
-console.log('array of employee data: ',  employees );
-
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
@@ -49,9 +47,82 @@ console.log('array of employee data: ',  employees );
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
+
+  // {
+  //   name: 'Atticus',
+  //   employeeNumber: '2405',
+  //   annualSalary: '47000',
+  //   reviewRating: 3
+  // },
+
   // your logic here
-  
+  console.log(employee.name)
+  let eName = employee.name
+  let eEmployeeNumber = employee.employeeNumber
+  let eAnnualSalary = Number(employee.annualSalary)
+  let eReviewRating = employee.reviewRating
+
+  let bonusPercentage = 0;
+
+  // If rating is <= 2, bonus = 0
+  if (eReviewRating <= 2) {
+    bonusPercentage = 0
+  }
+
+  // If rating is 3, bonus = 4
+  if (eReviewRating === 3) {
+    bonusPercentage = 4
+  }
+
+  // If rating is 4, bonus = 6
+  if (eReviewRating === 4) {
+    bonusPercentage = 6
+  }
+
+  // If rating is 5, bonus = 10
+  if (eReviewRating === 5) {
+    bonusPercentage = 10
+  }
+
+  // If employeeNumber.length is 4, bonus = bonus + 5
+  if (eEmployeeNumber.length === 4) {
+    bonusPercentage += 5
+  }
+
+  // If annualSalary > 65000, bonus = bonus - 1
+  if (eAnnualSalary > 65000) {
+    bonusPercentage -= 1
+  }
+
+  // If bonus > 13, bonus = 13
+
+  if (bonusPercentage > 13) {
+    bonusPercentage = 13
+  }
+
+  // If bonus < 0, bonus = 0
+  if (bonusPercentage < 0) {
+    bonusPercentage = 0
+  }
+
+  let totalBonus = (bonusPercentage/100) * eAnnualSalary
+
+  let totalCompensation = eAnnualSalary + totalBonus
+
   
   // return new object with bonus results
+  return {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus
+  }
+}
 
+
+for (let employee of employees) {
+  console.group()
+  // Call function for each of the employees and console log.
+  console.log(calculateIndividualEmployeeBonus(employee))
+  console.groupEnd()
 }
